2fd64eef851b9d76121d18909b04baeb67423fe0
ffekirnew <ffekirnew0808@gmail.com>
2024-03-24 20:47:25 +0300

    Configure swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('/api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Template')
+    .setDescription('API For the template project.')
+    .setVersion('0.1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
