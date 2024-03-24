1a691f0b988e53266a4666aeb947c38bc9e2ee6e
ffekirnew <ffekirnew0808@gmail.com>
2024-03-24 20:48:24 +0300

    Include the auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import config from 'src/config/app.config';
