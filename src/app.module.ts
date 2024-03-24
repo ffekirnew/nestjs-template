d9c28d3273cb0f48beeac13c7486cb4f7c10a978
ffekirnew <ffekirnew0808@gmail.com>
2024-03-24 20:46:49 +0300

    Configure app to load configuration at start

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from 'src/config/app.config';
+
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
   ],
   controllers: [],
   providers: [],
